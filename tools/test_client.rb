4f54d9956fb6e33b0b8bbeed6fcfddbd60d32b6a
Ubuntu <ubuntu@ip-172-31-33-240.ap-northeast-1.compute.internal>
2019-06-15 10:09:33 +0000

    add thrift client

--- tools/test_client.rb
+++ tools/test_client.rb
@@ -1,1 +1,27 @@
 puts 'tester'
+$:.push('../service/gen-rb')
+
+require 'thrift'
+require 'remote_stock'
+
+begin
+  	port = ARGV[0] || 9090
+
+	transport = Thrift::BufferedTransport.new(Thrift::Socket.new('localhost',9090))
+	protocol = Thrift::BinaryProtocol.new(transport)
+	client = RemoteStock::Client.new(protocol)
+
+	transport.open()
+
+	ar = RemoteStockOperation.new()    
+	ar.name = "Apple"
+	ar.number = 10
+	
+	# Run a remote calculation
+	result = client.sell_good(ar)  #it accessing the ruby server program method calc via thrift service
+	puts result.inspect
+	
+	transport.close()
+rescue
+  puts $!
+end
